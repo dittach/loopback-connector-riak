6a13765ab87c0c8655c407d4731378c1668f9d03
Dan Connor <danconn@danconnor.com>
2015-02-09 22:28:23 -0500

    updateAttributes tests

---
+++ test/api/update_attribute.js
@@ -0,0 +1,57 @@
+var sinon        = require('sinon');
+var loopbackRiak = require('../../lib/riak');
+var assert       = require('assert');
+
+describe("api/updateAttributes", function(){
+  var findStub, saveStub;
+  var datasource = { settings: {} };
+  var modelName = "TestModel";
+
+  var updateData = {
+    fresh: "data"
+  };
+
+  var id = "gobsmacked";
+
+  beforeEach(function(){
+    loopbackRiak.initialize(datasource);
+
+    findStub = sinon.stub(datasource.connector, "find");
+    saveStub = sinon.stub(datasource.connector.db, "save");
+  });
+
+  afterEach(function(){
+    findStub.restore();
+    saveStub.restore();
+  });
+
+  describe("if the document exists", function(){
+    it("it saves the document with the update data", function(done){
+      datasource.connector.updateAttributes(modelName, id, updateData, function(error, document){
+        var saveArgs = saveStub.lastCall.args;
+
+        assert.ok(document.fresh);
+        assert.equal(saveArgs[0], modelName);
+        assert.equal(saveArgs[1], id);
+        assert.equal(saveArgs[2].fresh, updateData.fresh);
+
+        done();
+      });
+
+      findStub.yieldsAsync(null, {});
+      saveStub.yieldsAsync(null, true);
+    });
+  });
+
+  describe("if the document doesn't exist", function(){
+    it("throws an error", function(done){
+      datasource.connector.updateAttributes(modelName, id, updateData, function(error, document){
+        assert.ok(error);
+
+        done();
+      });
+
+      findStub.yieldsAsync(null, null);
+    });
+  });
+});
