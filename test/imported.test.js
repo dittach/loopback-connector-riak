440226e2793e4da171869481883c7131cbf6e80f
Dan Connor <danconn@danconnor.com>
2015-04-20 12:09:33 -0400

    * add support for loading schemas when booting tests * fix errors being sent back for 404 responses * handle conditions to query string a bit better

--- test/imported.test.js
+++ test/imported.test.js
@@ -3,7 +3,10 @@ describe('mysql imported features', function () {
   before(function(done) {
     require('./init.js');
 
-    prepareDatabaseForTests(done);
+    prepareDatabaseForTests(function(){
+      console.log("running the actual tests now...");
+      done();
+    });
   });
 
   require('loopback-datasource-juggler/test/common.batch.js');
--- test/init.js
+++ test/init.js
@@ -1,3 +1,6 @@
+var async = require('async');
+var fs = require('fs');
+
 module.exports = require('should');
 
 var DataSource = require('loopback-datasource-juggler').DataSource;
@@ -20,23 +23,61 @@ global.getConfig = function (options) {
 
 global.getDataSource = global.getSchema = function (options) {
   var db = new DataSource(require('../'), getConfig(options));
+
+  var originalSearch = db.connector.search;
+  db.connector.search = function(){
+    var args = arguments;
+
+    setTimeout(function(){
+      originalSearch.apply(db.connector, args);
+    }, 8000); // yes, this makes the tests ridiculously slow
+  }
+
   return db;
 };
 
-global.prepareDatabaseForTests = function(callback){
-  var db = getSchema();
+var prepareBucketForSearching = function(db, bucketName, callback){
+  var indexName = bucketName.toLowerCase();
   var yz = db.connector.db.yokozuna;
 
-  console.log("creating a 'model' index for use in testing...");
+  var schemaXML = fs.readFileSync("./test/schemas/" + indexName + ".xml").toString('utf8');
 
-  yz.createIndex("model", "_yz_default", function(){
-    // give some time for the index to actually be created
-    setTimeout(function(){
-      console.log("associating the 'model' index with the 'Model' bucket...");
+  console.log("creating '", indexName, "' solr/yz schema...");
 
-      yz.associateIndexWithBucket("model", "Model", function(){
-        callback();
-      });
-    }, 3000);
+  // :( :( this is definitely not fun. basically because yokozuna is
+  // solr and solr is lucene, the interactions between riak and solr
+  // take indeterminate amounts of time and we have to basically give
+  // plennntttty of time for them to get in sync when we make changes
+  yz.createSchema(indexName, schemaXML, function(){
+    console.log("destroying '", indexName, "' index...");
+    yz.disassociateIndexFromBucket(bucketName, function(){
+      setTimeout(function(){
+        yz.destroyIndex(indexName, function(){
+          setTimeout(function(){
+            console.log("creating '", indexName, "' index...");
+
+            yz.createIndex(indexName, indexName, function(){
+              setTimeout(function(){
+                console.log("associating the '", indexName, "' index with the '", bucketName, "' bucket...");
+
+                yz.associateIndexWithBucket(indexName, bucketName, function(){
+                  setTimeout(function(){
+                    callback();
+                  }, 5000);
+                });
+              }, 10000); // creating an index takes an especially long time
+            });
+          }, 5000);
+        });
+      }, 5000);
+    });
   });
+}
+
+global.prepareDatabaseForTests = function(prepareCallback){
+  var db = getSchema();
+
+  async.each([ "Model", "User" ], function(bucketName, callback){
+    prepareBucketForSearching(db, bucketName, callback);
+  }, prepareCallback);
 }
